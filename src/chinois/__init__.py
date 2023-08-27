2c20dd609642e8d18b199a554a8ba37add34e467
Louis Maddox <louismmx@gmail.com>
2023-08-28 00:23:05 +0100

    build: bump -> v0.2.1

--- src/chinois/__init__.py
+++ src/chinois/__init__.py
@@ -23,7 +23,7 @@ from . import css_parser as cp
 from . import css_types as ct
 from .util import DEBUG, SelectorSyntaxError  # noqa: F401
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = [
     "DEBUG",
